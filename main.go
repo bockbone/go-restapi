2ec21601d696bb6195e51cd9582ea40aa4443f93
Hafizul Hassan <36685009+bockbone@users.noreply.github.com>
2021-06-27 20:03:24 +0800

    Add update and delete

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
@@ -62,12 +63,35 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 
 //Update book
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books,book) 
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
 	
 }
 
 //delete book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
 
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
@@ -86,6 +110,7 @@ func main() {
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
+	fmt.Println("Sercver is starting...")
 	log.Fatal(http.ListenAndServe(":8000",r))
 
 	
